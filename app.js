47da6d660669187cc5079df361991c31815f6bf4
andrianus16 <andrianus16@gmail.com>
2020-10-06 12:44:48 +0700

    updated

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json()); // creates http server
 
 app.get('/receive-webhook', (req, res) => {
-    console.log(res.body);
+    console.log("SELAMAT ANDAR TELAH CONNECT");
 });
 
 app.post('/add-webhook', (req, res) => {
-  console.log(req.body);
+  console.log("SELAMAT ANDAR TELAH CONNECT");
 });
 
 app.listen(3000, () => console.log('[Testing] Webhook is listening'));
