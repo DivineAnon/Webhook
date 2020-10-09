84efb30d3998e73d5e7788a3ec3db1cd0cb32e66
andrianus16 <andrianus16@gmail.com>
2020-10-09 14:53:30 +0700

    updated

--- app.js
+++ app.js
@@ -2,14 +2,60 @@
 
 const config = require("./config.js");
 const token = 'mcphooks', apiUrl = config.apiUrl;
-const app = require('express')();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
 const fetch = require('node-fetch');
+
+function loging(req, res, next) {
+  var logger = require('./middlewares/logger_winston');
+  let appName = process.env.APP_NAME;
+  let requestTime = new Date(Date.now());
+  let request = {
+    method: req.method,
+    url: req.url,
+    body: req.body
+  }
+  console.log(request);
+
+  let tmp = res.send;
+  res.send = function (data) {
+    let executionTime = (new Date() - requestTime) + 'ms';
+    let response = {
+      statusCode: res.statusCode,
+      body: (data) ? JSON.parse(data) : null
+    };
+    let log = {
+      appName,
+      requestTime: dateFormat(requestTime, "yyyy-mm-dd HH:MM:ss"),
+      executionTime,
+      request,
+      response
+    }
+    // log level error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
+    logger.log('info', JSON.stringify(log));
+    tmp.apply(res, arguments);
+  }
+  next();
+}
+
+var app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(loging);
+app.use(require('sanitize').middleware);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-process.on('unhandledRejection', err => {
-  console.log(err)
-});	
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/receive-webhook', (req, res) => {
   return res.status(200).send(req.body);
